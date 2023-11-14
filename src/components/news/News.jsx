e19dac9945ff32fac028e671eb682c90a49ca0a1
Bhoomika <bhoomikahm18@gmail.com>
2023-11-14 11:28:09 +0530

    Adding News components

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import NewsItem from '../newsItem/NewsItem.jsx'
 
 export class News extends Component {
   render() {
     return (
-      <div>News</div>
+        <div className='container my-3'>
+          <h2>NewsMonkey - Top Headlines</h2>
+          <div className='row'>
+            <div className='col-md-4'>
+              <NewsItem title="my title" discription="abcd" />
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="my title" discription="abcd" />
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="my title" discription="abcd" />
+            </div>
+          </div>
+        </div>
     )
   }
 }
