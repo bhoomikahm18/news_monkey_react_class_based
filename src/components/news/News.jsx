273b7c1aac7953c1aacf5397a0b7283d8625b34b
Bhoomika <bhoomikahm18@gmail.com>
2023-11-16 11:04:10 +0530

    Substittuting image for null image

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -60,15 +60,23 @@ export class News extends Component {
         {this.state.loading && <Spinner />}
         {(!this.state.loading) &&
 
-          <div className='row'>
-            {(this.state.articles.map((item, index) => {
-              return (<div className='col-md-4' key={index}>
-                <NewsItem
-                  title={item.title}
-                  discription={item.description}
-                  imageUrl={item.urlToImage} />
-              </div>)
-            }))
+          <div className="row">
+            {
+              this.state.articles.map((item, index) => {
+                return (
+                  <div className="col-md-4" key={index}>
+                    <NewsItem
+                      title={item.title.slice(0, 45)}
+                      decription={(item.description) ? item.description.slice(0, 88) : "Click for more Details"}
+                      imageUrl={(item.urlToImage != null) ? item.urlToImage : "https://theleaflet.in/wp-content/uploads/2021/09/IT-Dept.jpg"}
+                      newsURL={item.url}
+                      author={item.author}
+                      date={item.publishedAt}
+                      source={item.source.name}
+                    />
+                  </div>
+                )
+              })
             }
           </div>
         }
