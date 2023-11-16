10838a659b8c5dc993eca236bbde428803d5f536
Bhoomika <bhoomikahm18@gmail.com>
2023-11-16 11:07:42 +0530

    Refactoring duplicate code

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -29,17 +29,19 @@ export class News extends Component {
     this.handlePreviousClick = this.handlePreviousClick.bind(this);
   }
 
-  componentDidMount() {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bd002a1cf4f64969a8819fcbfe3a190f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
-    ).then(res => res.json()).then(data => {
-      this.setState({
-        ...this.state,
-        articles: data.articles,
-        totalArticles: data.totalResuls,
-        loading: false
-      });
+  async updateNews(pageNo) {
+    let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bd002a1cf4f64969a8819fcbfe3a190f&page=${pageNo}&pageSize=${this.props.pageSize}`);
+    let data = await res.json();
+    this.setState({
+      articles: data.articles,
+      loading: false,
+      page: pageNo,
+      totalArticles: data.totalResults
     });
+  }
+
+  componentDidMount() {
+    this.updateNews(1);
   }
   async handleNextClick() {
     let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bd002a1cf4f64969a8819fcbfe3a190f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`);
